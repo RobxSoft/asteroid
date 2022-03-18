b57f86cd62838ba116b226eff92f3c8e76e2380f
RobxSoft <48138370+RobxSoft@users.noreply.github.com>
2022-03-18 10:34:51 +0100

    draw/update

--- Headers/glob.h
+++ Headers/glob.h
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
 const int SIZE_X = 20;
 const int SIZE_Y = 6;
-char *GAME[SIZE_X][SIZE_Y];
+char *GAME[SIZE_Y][SIZE_X];
--- main.c
+++ main.c
@@ -1,31 +1,68 @@
 #include "Headers/glob.h"
 
+void spawnAsteroid(){
+    srand(time(NULL)); 
+    int r = rand() % SIZE_Y;
+    GAME[r][SIZE_X-1] = "*";
+}
+
+void update(){
+    for(int y=SIZE_Y-1; y > 0; y--){
+        for(int x=SIZE_X-1; x > 0; x--){
+            if(strcmp(GAME[y][x], "*") == 0){
+                GAME[y][x] = " ";
+                if(x>0){
+                    GAME[y][x-5] = "*";
+                }
+            }else if (strcmp(GAME[y][x], "-") == 0){
+                GAME[y][x] = " ";
+                if(x<SIZE_X){
+                    GAME[y][x+1] = "-";
+                }
+            }
+        }
+    }
+}
+
 void startGame(){
-    for(int x; x < SIZE_X+1; x++){
-        for(int y; y < SIZE_Y; y++){
+    for(int y=0; y < SIZE_Y+1; y++){
+        for(int x=0; x < SIZE_X; x++){
             if(x==0 && y==0){
-                GAME[x][y] = ">";
+                GAME[y][x] = ">";
             }else{
-                GAME[x][y] = " ";
+                GAME[y][x] = " ";
             }
         }
     }
 }
 
 void draw(){
-    for(int x; x < SIZE_X+1; x++){
-        for(int y; y < SIZE_Y; y++){
-            if(x==1){
-                printf("%d", y);
+    for(int y=0; y < SIZE_Y; y++){
+        for(int x=0; x < SIZE_X+1; x++){
+            if(x==0){
+                printf("%d", y+1);
             }else{
-                printf("%c", GAME[x][y]);
+                printf("%c", *GAME[y][x-1]);
             }
         }
+        printf("\n");
     }
 }
 
 int main(void){
     startGame();
+    spawnAsteroid();
     draw();
+
+    printf("\n\n");
+
+    update();
+    draw();
+    
+    printf("\n\n");
+
+    update();
+    draw();
+
     return 1;
 }
